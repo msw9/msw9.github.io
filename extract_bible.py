7164494b2756984d73a393d59736d3b1889d1b27
Alex <navnicooo@protonmail.com>
2025-12-11 00:49:17 +0100

    extraction terminée ?

--- extract_bible.py
+++ extract_bible.py
@@ -1,9 +1,8 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
 import requests
-import os
 import re
-import pickle
+import json
 
 def get_html(url):
     r = requests.get(url)
@@ -20,44 +19,42 @@ def clean_fr(lst):
     lst = re.split(r'\d+',''.join(lst))
     return lst.remove('')
 
-url = "https://www.bible.com/bible/3438/GEN.1.MYE?parallel=93"
-"""
-if os.path.isfile("gen_1_2.pkl"):
-    with open ("gen_1_2.pkl","rb") as f:
-        soup = pickle.load(f)
-else:            
-    soup = BeautifulSoup(get_html(url),'html.parser')
-    with open ("gen_1_2.pkl","wb") as f:
-        pickle.dump(soup,f)
-"""
 data = {'Myènè':[],'Français':[]}
 
-for i in range (1,3):
-    url = f"https://www.bible.com/bible/3438/GEN.{i}.MYE?parallel=93"
-    soup = BeautifulSoup(get_html(url),'html.parser')
+"""
+Requête
+GET:https://www.bible.com/api/bible/version/3438
+"""
+with open ('response.json',encoding='utf-8') as f:
+    response = json.loads(f.read())
     
-    #Séparer la partie fr/mye
-    mye= soup.find(lang="mye")
-    fr=soup.find(lang="fr")
+for x in response['books']:
+    for y in x['chapters']:
+        url = f"https://www.bible.com/bible/3438/{y['usfm']}.MYE?parallel=93"
+        soup = BeautifulSoup(get_html(url),'html.parser')
+        
+        #Séparer la partie fr/mye
+        mye= soup.find(lang="mye")
+        fr=soup.find(lang="fr")
 
-    #Extraire versets Gabonais
-    m_verses = mye.find_all('span')
-    m_verses = clean_mye(list(m_verses))
+        #Extraire versets Gabonais
+        m_verses = mye.find_all('span')
+        m_verses = clean_mye(list(m_verses))
 
-    #Extraire les versets Français
-    f_verses = fr.find_all('span',{'class':['ChapterContent_content__RrUqA','ChapterContent_label__R2PLt']})# ou ('span',class_= False)
-    f_verses = [x.get_text() for x in list(f_verses)]
-    f_verses.remove(' ')
-    f_verses = list(map(lambda x:"" if x=="#" else x,f_verses))
-    f_verses = re.split(r'\d+',''.join(f_verses))
-    f_verses.remove('')
+        #Extraire les versets Français
+        f_verses = fr.find_all('span',{'class':['ChapterContent_content__RrUqA','ChapterContent_label__R2PLt']})# ou ('span',class_= False)
+        f_verses = [x.get_text() for x in list(f_verses)]
+        f_verses.remove(' ')
+        f_verses = list(map(lambda x:"" if x=="#" else x,f_verses))
+        f_verses = re.split(r'\d+',''.join(f_verses))
+        f_verses.remove('')
 
-    print((len(m_verses),len(f_verses))) #/!\ CHECK POINT
-    
-    if len(m_verses) == len(f_verses):
-        results = list(zip(m_verses,f_verses))
-        data['Myènè'].extend(m_verses)
-        data['Français'].extend(f_verses)
+        print((len(m_verses),len(f_verses))) #/!\ CHECK POINT
+        
+        if len(m_verses) == len(f_verses):
+            results = list(zip(m_verses,f_verses))
+            data['Myènè'].extend(m_verses)
+            data['Français'].extend(f_verses)
 
 df = pd.DataFrame.from_dict(data)
 df.to_csv('resultats.csv')
